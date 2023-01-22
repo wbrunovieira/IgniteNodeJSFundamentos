22c1794db08b9fc686f3c423be87dede15279b4f
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-22 15:18:28 -0300

    added others routes

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,27 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users')
+      const { search } = req.query
+
+      const users = database.select('users', {
+        name: search,
+        email: search
+      })
 
       return res.end(JSON.stringify(users))
     }
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -30,4 +36,30 @@ export const routes = [
       return res.writeHead(201).end()
     }
   },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email,
+      })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
+  }
 ]
