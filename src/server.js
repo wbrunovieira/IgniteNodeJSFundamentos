5632d995df8ea141ded6d59660e49d08aa9056d2
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-22 08:16:03 -0300

    buffers and middlewares in streams

--- src/server.js
+++ src/server.js
@@ -1,29 +1,31 @@
-import http from 'node:http';
+import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
+      name,
+      email,
     })
 
-    return res.writeHead(201).end('usuario criado')
+    return res.writeHead(201).end()
   }
 
   return res.writeHead(404).end()
 })
 
 server.listen(3333)
-
-
