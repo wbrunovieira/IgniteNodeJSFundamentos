083f1b4410da34f8cd94b948a3f011d3525e17cc
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-22 04:56:07 -0300

    status code and headers

--- src/server.js
+++ src/server.js
@@ -1,19 +1,29 @@
 import http from 'node:http';
 
-const server = http.createServer( ( req, res) => {
+const users = []
 
-    const { method, url } = req
+const server = http.createServer((req, res) => {
+  const { method, url } = req
 
-    if (method === 'GET' && url === '/users') {
-      return res.end('Listagem de usuários')
-    }
-  
-    if (method === 'POST' && url === '/users') {
-      return res.end('Criação de usuário')
-    }
-    
-    return res.end('viva')
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
+    return res.writeHead(201).end('usuario criado')
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
+
 
