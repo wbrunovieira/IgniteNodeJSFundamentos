29dd6a37622942e1c686b1c015f26d75876a719b
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-21 21:19:45 -0300

    created a server without framework

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import http from 'node:http';
+
+const server = http.createServer( ( req, res) => {
+
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+      return res.end('Listagem de usuários')
+    }
+  
+    if (method === 'POST' && url === '/users') {
+      return res.end('Criação de usuário')
+    }
+    
+    return res.end('viva')
+})
+
+server.listen(3333)
+
