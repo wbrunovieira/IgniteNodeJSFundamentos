dab18d884e36737efec3867b36a190cfa6288eec
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-22 07:27:09 -0300

    for await, wait finish the stream for return

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -7,7 +7,7 @@ class OneToHundredStream extends Readable {
     const i = this.index++
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i))
@@ -21,5 +21,9 @@ class OneToHundredStream extends Readable {
 fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundredStream(),
-  duplex: 'half' 
+  duplex:'half'
+}).then(res => {
+  return res.text()
+}).then(data => {
+  console.log(data)
 })
